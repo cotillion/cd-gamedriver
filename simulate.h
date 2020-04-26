cb2ddac6b4a336de938f81e76e1955dd1c1f76b7
Erik Gävert <erik@gavert.net>
2020-04-26 12:56:15 +0200

    Mark error() and fatal() as noreturn to assist analyser

--- simulate.h
+++ simulate.h
@@ -2,6 +2,6 @@ struct object;
 
 void update_actions(struct object *aob);
 char *get_gamedriver_info(char *str);
-void fatal(char *fmt, ...) __attribute__((format(printf, 1, 2)));
+void fatal(char *fmt, ...) __attribute__((format(printf, 1, 2))) __attribute__ ((noreturn));
 void warning(char *fmt, ...) __attribute__((format(printf, 1, 2)));
-void error(char *fmt, ...) __attribute__((format(printf, 1, 2)));
+void error(char *fmt, ...) __attribute__((format(printf, 1, 2))) __attribute__ ((noreturn));
