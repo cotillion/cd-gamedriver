03548c4d79cf697a64bf7bd5b4c9c047a752addc
Erik Gävert <erik@gavert.net>
2021-04-02 12:31:12 +0200

    Add some missing irregular plurals to plural_word
    - Fixes a stack overflow
    - Switch to a mapping to store the irregular plurals

--- clib/gl_language.c
+++ clib/gl_language.c
@@ -7,14 +7,12 @@
 #include "../lint.h"
 #include "../interface.h"
 #include "../object.h"
+#include "../mapping.h"
+
 /* Define variables */
 extern struct object *previous_ob;
 
 /* Define functions */
-
-#ifdef _SEQUENT_
-extern void *alloca(unsigned);
-#endif
 
 char lower(char c)
 {
@@ -52,72 +50,222 @@ global_article(struct svalue *fp)
         return;
     }
     if (tmp[0] == 'a' || tmp[0] == 'e' || tmp[0] == 'i' ||
-	tmp[0] == 'o' || tmp[0] == 'u')
+        tmp[0] == 'o' || tmp[0] == 'u')
     {
         push_string("an", STRING_CSTRING);
-	return;
+        return;
     }
     push_string("a", STRING_CSTRING);
     return;
 }
 
-static func func_article = 
+static func func_article =
 {
     "article",
     global_article,
 };
 
+static char *irregular_plurals[][2] = {
+    { "addendum", "addenda" },
+    { "aircraft", "aircraft" },
+    { "alga", "algae" },
+    { "alumna", "alumnae" },
+    { "alumnus", "alumni" },
+    { "amoeba", "amoebae" },
+    { "analysis", "analyses" },
+    { "antenna", "antennae" },
+    { "antithesis", "antitheses" },
+    { "apex", "apices" },
+    { "appendix", "appendices" },
+    { "automaton", "automata" },
+    { "axis", "axes" },
+    { "bacillus", "bacilli" },
+    { "bacterium", "bacteria" },
+    { "barracks", "barracks" },
+    { "basis", "bases" },
+    { "beau", "beaux" },
+    { "bison", "bison" },
+    { "buffalo", "buffalo" },
+    { "bureau", "bureaus" },
+    { "cactus", "cacti" },
+    { "calf", "calves" },
+    { "carp", "carp" },
+    { "census", "censuses" },
+    { "chassis", "chassis" },
+    { "cherub", "cherubim" },
+    { "child", "children" },
+    { "château", "châteaus" },
+    { "cod", "cod" },
+    { "codex", "codices" },
+    { "concerto", "concerti" },
+    { "corpus", "corpora" },
+    { "crisis", "crises" },
+    { "criterion", "criteria" },
+    { "curriculum", "curricula" },
+    { "datum", "data" },
+    { "deer", "deer" },
+    { "diagnosis", "diagnoses" },
+    { "die", "dice" },
+    { "dwarf", "dwarves" },     /* We use the tolkien plural */
+    { "echo", "echoes" },
+    { "elf", "elves" },
+    { "elk", "elk" },
+    { "ellipsis", "ellipses" },
+    { "embargo", "embargoes" },
+    { "emphasis", "emphases" },
+    { "erratum", "errata" },
+    { "faux pas", "faux pas" },
+    { "fez", "fezes" },
+    { "firmware", "firmware" },
+    { "fish", "fish" },
+    { "focus", "foci" },
+    { "foot", "feet" },
+    { "formula", "formulae" },
+    { "fungus", "fungi" },
+    { "gallows", "gallows" },
+    { "genus", "genera" },
+    { "goose", "geese" },
+    { "graffito", "graffiti" },
+    { "grouse", "grouse" },
+    { "half", "halves" },
+    { "hero", "heroes" },
+    { "hoof", "hooves" },
+    { "hovercraft", "hovercraft" },
+    { "hypothesis", "hypotheses" },
+    { "index", "indices" },
+    { "kakapo", "kakapo" },
+    { "knife", "knives" },
+    { "larva", "larvae" },
+    { "leaf", "leaves" },
+    { "libretto", "libretti" },
+    { "life", "lives" },
+    { "loaf", "loaves" },
+    { "locus", "loci" },
+    { "louse", "lice" },
+    { "man", "men" },
+    { "matrix", "matrices" },
+    { "means", "means" },
+    { "medium", "media" },
+    { "media", "media" },
+    { "memorandum", "memoranda" },
+    { "millennium", "millennia" },
+    { "minutia", "minutiae" },
+    { "moose", "moose" },
+    { "mouse", "mice" },
+    { "nebula", "nebulae" },
+    { "nemesis", "nemeses" },
+    { "neurosis", "neuroses" },
+    { "news", "news" },
+    { "nucleus", "nuclei" },
+    { "oasis", "oases" },
+    { "offspring", "offspring" },
+    { "opus", "opera" },
+    { "ovum", "ova" },
+    { "ox", "oxen" },
+    { "paralysis", "paralyses" },
+    { "parenthesis", "parentheses" },
+    { "person", "people" },
+    { "phenomenon", "phenomena" },
+    { "phylum", "phyla" },
+    { "pike", "pikes" },            /* We use the plural form of the weapon */
+    { "polyhedron", "polyhedra" },
+    { "potato", "potatoes" },
+    { "prognosis", "prognoses" },
+    { "quiz", "quizzes" },
+    { "radius", "radii" },
+    { "referendum", "referenda" },
+    { "salmon", "salmon" },
+    { "scarf", "scarves" },
+    { "self", "selves" },
+    { "series", "series" },
+    { "sheep", "sheep" },
+    { "shelf", "shelves" },
+    { "shrimp", "shrimp" },
+    { "spacecraft", "spacecraft" },
+    { "species", "species" },
+    { "spectrum", "spectra" },
+    { "squid", "squid" },
+    { "stimulus", "stimuli" },
+    { "stratum", "strata" },
+    { "swine", "swine" },
+    { "syllabus", "syllabi" },
+    { "symposium", "symposia" },
+    { "synopsis", "synopses" },
+    { "synthesis", "syntheses" },
+    { "tableau", "tableaus" },
+    { "that", "those" },
+    { "thesis", "theses" },
+    { "thief", "thieves" },
+    { "this", "these" },
+    { "tomato", "tomatoes" },
+    { "tooth", "teeth" },
+    { "trout", "trout" },
+    { "tuna", "tuna" },
+    { "vertebra", "vertebrae" },
+    { "vertex", "vertices" },
+    { "veto", "vetoes" },
+    { "vita", "vitae" },
+    { "vortex", "vortices" },
+    { "watercraft", "watercraft" },
+    { "wharf", "wharves" },
+    { "wife", "wives" },
+    { "wolf", "wolves" },
+    { "woman", "women" },
+};
+
+struct mapping *init_irregular_plural_word_map()
+{
+    struct mapping *map = allocate_map(128);
+    struct svalue key, value;
+
+    for (size_t i = 0; i < sizeof(irregular_plurals) / sizeof(char *) / 2; i++) {
+        key.type = T_STRING;
+        key.string_type = STRING_CSTRING;
+        key.u.string = irregular_plurals[i][0];
+
+        value.type = T_STRING;
+        value.string_type = STRING_CSTRING;
+        value.u.string = irregular_plurals[i][1];
+
+        assign_svalue(get_map_lvalue(map, &key, 1), &value);
+    }
+
+    return map;
+}
+
+static struct mapping *plural_map = NULL;
+
 static void
 global_plural_word(struct svalue *fp)
 {
-    char *tmp, *str, ultimate, penultimate;
-    size_t sl;
-    
-    if (fp[0].type != T_STRING)
-    {
-	push_number(0);
-	return;
-    }
-    str = fp[0].u.string;
+    if (NULL == plural_map)
+        plural_map = init_irregular_plural_word_map();
 
-    if (strcmp(str, "tooth") == 0)
+    if (fp->type != T_STRING)
     {
-	push_string("teeth", STRING_CSTRING); return;
+	    push_number(0);
+	    return;
     }
-    else if (strcmp(str, "foot") == 0)
+
+    char *str = fp->u.string;
+    struct svalue *match = get_map_lvalue(plural_map, fp, 0);
+    if (match->type == T_STRING)
     {
-	push_string("feet", STRING_CSTRING); return;
+        push_svalue(match);
+        return;
     }
-    else if (strcmp(str, "man") == 0)
+
+    size_t sl = strlen(str);
+    if (sl < 3 || sl > 100)
     {
-	push_string("men", STRING_CSTRING); return;
+        push_svalue(fp);
+	    return;
     }
-    else if (strcmp(str, "woman") == 0)
-    {
-	push_string("women", STRING_CSTRING); return;
-    }
-    else if (strcmp(str, "child") == 0)
-    {
-	push_string("children", STRING_CSTRING); return;
-    }
-    else if (strcmp(str, "sheep") == 0)
-    {
-	push_string("sheep", STRING_CSTRING); return;
-    }
-    else if (strcmp(str, "key") == 0)
-    {
-	push_string("keys", STRING_CSTRING); return;
-    }
-    
-    if ((sl = strlen(str)) < 3)
-    {
-	push_string(str, fp[0].string_type);
-	return;
-    }
-    tmp = alloca((strlen((char *)str) + 10));
+
+    char *tmp = alloca(sl + 10);
     (void)strncpy(tmp, str, sl);
-    ultimate = str[sl - 1];
-    penultimate = str[sl - 2];
+    char ultimate = str[sl - 1];
+    char penultimate = str[sl - 2];
     tmp[sl] = '\0';
 
     switch(ultimate)
@@ -161,7 +309,7 @@ global_plural_word(struct svalue *fp)
     return;
 }
 
-static func func_plural_word = 
+static func func_plural_word =
 {
     "plural_word",
     global_plural_word,
@@ -179,7 +327,7 @@ static var *(vars[]) =
    0,
 };
 
-struct interface gl_language = 
+struct interface gl_language =
 {
     "sys/global/language.c",
     vars,
