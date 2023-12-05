cotillion/cd-gamedriver

commit eb41365233c19537e2d2971cab0007780abfabaf

Fix compiler warning

lex.c
@@ -1909,7 +1909,7 @@ add_input(char *p)
     }
     outp -= l;
     nbuf += l;
-    (void)strncpy(outp, p, l);
+    strcpy(outp, p);
 }
 
 #define DEFHASH 1999

commit 3c8feee4d92e89a9e8ae19f1100fdc1dbad80b3f

Fix `#define` parsing bug
Commit eb41365233c19537e2d2971cab0007780abfabaf introduced a bug in
`#define` parsing wherein a null terminator would be written into the
parse input stream after the expanded definition. The intention of the
buggy commit was to eliminate a (wise) warning about using the source
length as the buffer length in a `strncpy` operation; using memcpy here
is probably the more-correct solution as we already know all the lengths
involved.

lex.c
@@ -1909,7 +1909,7 @@ add_input(char *p)
     }
     outp -= l;
     nbuf += l;
-    strcpy(outp, p);
+    memcpy(outp, p, l);
 }
 
 #define DEFHASH 1999